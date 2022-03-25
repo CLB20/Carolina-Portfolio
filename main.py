ba160075e4f78447e66900138c2e1d14cd76b6d2
CLB20 <carolina.lascano94@hotmail.com>
2022-03-25 14:21:15 -0500

    Update environ variables

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
-
+FORM_URL = os.environ.get("FORM_URL")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", form_url=FORM_URL)
 
 @app.route("/thanks")
 def thanks():
